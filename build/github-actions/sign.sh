6d9bdfdcf53d801dc626888e06b0fbe6549f74bf
Adam Wood <adamwoodce@gmail.com>
2021-08-19 13:35:22 +0100

    [WFX-187] Define java location.

--- build/github-actions/sign.sh
+++ build/github-actions/sign.sh
@@ -8,11 +8,11 @@ TOPDIR=$GITHUB_WORKSPACE
 pushd objdir-classic/dist/install/sea/
 7z x waterfox-classic-$BROWSER_VERSION.en-US.win64.installer.exe
 rm -f waterfox-classic-$BROWSER_VERSION.en-US.win64.installer.exe
-find ./ -type f -name "*.exe" -exec java -jar $JSIGN --storetype AZUREKEYVAULT --keystore "$AZURE_VAULT_ID" --alias "$AZURE_CRT" --tsaurl "http://rfc3161timestamp.globalsign.com/advanced" --tsmode RFC3161 --alg SHA-256 --storepass "$(az account get-access-token --resource "https://vault.azure.net" --tenant $AZURE_TENANT_ID | jq -r .accessToken)" {} \;
-find ./ -type f -name "*.dll" -exec java -jar $JSIGN --storetype AZUREKEYVAULT --keystore "$AZURE_VAULT_ID" --alias "$AZURE_CRT" --tsaurl "http://rfc3161timestamp.globalsign.com/advanced" --tsmode RFC3161 --alg SHA-256 --storepass "$(az account get-access-token --resource "https://vault.azure.net" --tenant $AZURE_TENANT_ID | jq -r .accessToken)" {} \;
+find ./ -type f -name "*.exe" -exec $JAVA_LOC -jar $JSIGN --storetype AZUREKEYVAULT --keystore "$AZURE_VAULT_ID" --alias "$AZURE_CRT" --tsaurl "http://rfc3161timestamp.globalsign.com/advanced" --tsmode RFC3161 --alg SHA-256 --storepass "$(az account get-access-token --resource "https://vault.azure.net" --tenant $AZURE_TENANT_ID | jq -r .accessToken)" {} \;
+find ./ -type f -name "*.dll" -exec $JAVA_LOC -jar $JSIGN --storetype AZUREKEYVAULT --keystore "$AZURE_VAULT_ID" --alias "$AZURE_CRT" --tsaurl "http://rfc3161timestamp.globalsign.com/advanced" --tsmode RFC3161 --alg SHA-256 --storepass "$(az account get-access-token --resource "https://vault.azure.net" --tenant $AZURE_TENANT_ID | jq -r .accessToken)" {} \;
 7z a -r -t7z app.7z -mx -m0=BCJ2 -m1=LZMA:d25 -m2=LZMA:d19 -m3=LZMA:d19 -mb0:1 -mb0s1:2 -mb0s2:3
 cp $BUILD_DIR/browser/installer/windows/app.tag .
 cp $BUILD_DIR/other-licenses/7zstub/firefox/7zSD.sfx .
 cat 7zSD.sfx app.tag app.7z > "Waterfox Classic $BROWSER_VERSION Setup.exe"
-$SIGNTOOL sign -tr http://timestamp.digicert.com -fd sha256 -f "$BUILD_DIR/build/appveyor/waterfox.cer" -csp "eToken Base Cryptographic Provider" -kc "[{{$CSP}}]=te-ba4d65f7-af06-4aa4-91c1-54d4f0cb9b5b" "Waterfox Classic $BROWSER_VERSION Setup.exe"
+$JAVA_LOC -jar $JSIGN --storetype AZUREKEYVAULT --keystore "$AZURE_VAULT_ID" --alias "$AZURE_CRT" --tsaurl "http://rfc3161timestamp.globalsign.com/advanced" --tsmode RFC3161 --alg SHA-256 --storepass "$(az account get-access-token --resource "https://vault.azure.net" --tenant $AZURE_TENANT_ID | jq -r .accessToken)" "Waterfox Classic $BROWSER_VERSION Setup.exe"
 popd
